ec1c9a117ef294d2af3c06b3859ba88a181b3d95
farazzkamali <farazzkamali80@gmail.com>
2023-10-14 16:33:33 +0330

    feat: style active link in navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,8 +1,15 @@
+'use client'
+
 import Link from 'next/link'
 import React from 'react'
+import { usePathname } from 'next/navigation'
 import {AiFillBug} from 'react-icons/ai'
+import classnames from 'classnames'
 
 const NavBar = () => {
+
+    const currentPath = usePathname()
+
     const links = [
         {lable:'Dashboard', href:'/'},
         {lable:'Issues', href:'/issues'}
@@ -12,7 +19,11 @@ const NavBar = () => {
     <nav className='flex space-x-6 px-5 border-b mb-5 h-14 items-center'>
         <Link href='/'><AiFillBug/></Link>
         <ul className='flex space-x-6'>
-            {links.map(link=><Link key={link.href} href={link.href} className=' text-zinc-500 hover:text-zinc-800 transition-colors'>{link.lable}</Link>)}
+            {links.map(link=><Link key={link.href} href={link.href} className={classnames({
+                'text-zinc-900':link.href === currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors': true
+            })}>{link.lable}</Link>)}
         </ul>
     </nav>
   )
---
+++ app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IsuuesPge = () => {
+  return (
+    <div>IsuuesPge</div>
+  )
+}
+
+export default IsuuesPge
