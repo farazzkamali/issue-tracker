bdfce63ac88a85e9ed4e0e5e6f6031f66c9aaacf
farazzkamali <farazzkamali80@gmail.com>
2023-10-19 23:59:42 +0330

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,7 +4,7 @@ import IssueStatusBadge from '../components/IssueStatusBadge'
 import IssueActions from './IssueActions'
 
 import delay from 'delay'
-import Link from 'next/link'
+import Link from '../components/Link'
 
 const IsuuesPge = async() => {
   const issues = await prisma.issue.findMany()
