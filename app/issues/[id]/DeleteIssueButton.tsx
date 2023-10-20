bc18b494d59f91cf5e87ee482eb4e718e1cfbbcc
farazzkamali <farazzkamali80@gmail.com>
2023-10-20 19:05:08 +0330

    Show a conformation dialog box

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,9 +1,31 @@
-import { Button } from '@radix-ui/themes'
+'use client'
+import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import React from 'react'
 
 const DeleteIssueButton = ({issueId}:{issueId:number}) => {
   return (
-    <Button color='red'>Delete Issue</Button>
+
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+      <Button color='red'>Delete Issue</Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>
+            Confirm Deletion
+          </AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue? This action can not be undone.
+          </AlertDialog.Description>
+          <Flex gap="3" mt="4">
+            <AlertDialog.Cancel>
+              <Button color='gray' variant='soft'>Cancel</Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color='red'>Delete Issue</Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+    </AlertDialog.Root>
   )
 }
 
